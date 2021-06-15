b1129e149997949af7e986030294ea7b2b2c05c8
Khrystyna Leshko <christinelechko@gmail.com>
2021-06-15 19:28:19 +0200

    changing video background

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,7 +5,7 @@ function HomePage() {
     <div className="wrapper">
       <video autoPlay loop muted>
         <source
-          src="https://res.cloudinary.com/dertdncse/video/upload/v1623507603/smart%20portal/videos/london_edited_s7bg9n.mp4"
+          src="https://res.cloudinary.com/dgmm3pkuc/video/upload/v1623777768/react-smart-portal/British_Flag_Waving_HD_ie8lyx.mp4"
           type="video/mp4"
         />
       </video>
