34121d1974ecaab4460d8097f3b12cfe9781089f
luisjhc <ljoaquincuenca@gmail.com>
2021-06-17 21:18:36 +0200

    nav bar style done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,39 +8,43 @@ const Navbar = (props) => {
   const { user } = props;
 
   return (
-    <nav className="nav-container">
+    <nav>
       {props.user ? (
-        <>
-          <div className="nav-userGreeting">
-            <img
-              src={user.profilePic}
-              className="nav-userGreeting-img"
-              alt={`Greeting pic for ${user.username}`}
-            />
-            <h3>Welcome {user.username}</h3>
-          </div>
-          <div className="nav__authLinks">
-            <Link to={PATHS.MYPORTAL} className="nav-authLink">
-              &#127891; My Portal
-            </Link>
-            <Link to={PATHS.MYPROFILE} className="nav-authLink">
-              My Profile
-            </Link>
-          </div>
-          <div className="nav-logOut">
-            <button className="nav-logout-btn" onClick={props.handleLogout}>
-              Logout
-            </button>
-          </div>
-        </>
+        <div className="nav-container">
+          <>
+            <div className="nav-userGreeting">
+              <img
+                src={user.profilePic}
+                className="nav-userGreeting-img"
+                alt={`Greeting pic for ${user.username}`}
+              />
+              <p>Welcome {user.username}</p>
+            </div>
+            <div className="nav__authLinks">
+              <Link to={PATHS.MYPORTAL} className="nav-authLink">
+                &#127891; My Portal
+              </Link>
+              <Link to={PATHS.MYPROFILE} className="nav-authLink">
+                My Profile
+              </Link>
+            </div>
+            <div className="nav-logOut">
+              <button className="nav-logout-btn" onClick={props.handleLogout}>
+                Logout
+              </button>
+            </div>
+          </>
+        </div>
       ) : (
         <>
-          <Link to={PATHS.SIGNUPPAGE} className="nav-authLink">
-            Signup
-          </Link>
-          <Link to={PATHS.LOGINPAGE} className="nav-authLink">
-            Log In
-          </Link>
+          <div className="nav-loggedOut">
+            <Link to={PATHS.SIGNUPPAGE} className="nav-authLink">
+              Signup
+            </Link>
+            <Link to={PATHS.LOGINPAGE} className="nav-authLink">
+              Log In
+            </Link>
+          </div>
         </>
       )}
     </nav>
