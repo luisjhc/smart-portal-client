97fb9839b11cab880c32c6b0df095e5f0b133162
luisjhc <ljoaquincuenca@gmail.com>
2021-06-19 10:27:10 +0200

    styling...

--- src/pages/teacher/Content.jsx
+++ src/pages/teacher/Content.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 import * as PATHS from "../../utils/paths";
 import { ReactComponent as Beginner } from "../../Ilustrations/beginner.svg";
+import { ReactComponent as Intermediate } from "../../Ilustrations/intermediate.svg";
+import { ReactComponent as Advanced } from "../../Ilustrations/advanced.svg";
 import "./teacherCss/Content.css";
 
 function Content() {
@@ -11,11 +13,13 @@ function Content() {
         <Link to={PATHS.CONTENT_BEGINNER}>BEGINNER CONTENT</Link>
         <Beginner />
       </div>
-      <div>
+      <div className="content-intermediate">
         <Link to={PATHS.CONTENT_INTERMEDIATE}>INTERMEDIATE CONTENT</Link>
+        <Intermediate />
       </div>
-      <div>
+      <div className="content-advanced">
         <Link to={PATHS.CONTENT_ADVANCED}>ADVANCED CONTENT</Link>
+        <Advanced />
       </div>
     </div>
   );
