7801e3d7d0a011c40503eb0f536935c4a29523e8
luisjhc <ljoaquincuenca@gmail.com>
2021-06-19 16:37:04 +0200

    styling footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
+import "./Footer.css";
 
 export default function Footer() {
-  return <div>FOOTER | 2021</div>;
+  return <div className="main">© CREATED BY KHRYS AND LUIS | 2021</div>;
 }
